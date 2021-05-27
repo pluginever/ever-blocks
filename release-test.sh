1dc45cde71d58939cd51831aa0155082e30e581b
sultann <manikdrmc@gmail.com>
2021-05-27 14:47:43 +0600

    add buidl tester

---
+++ release-test.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --version*|-v*)
+      if [[ "$1" != *=* ]]; then shift; fi # Value is next arg if no `=`
+      VERSION="${1#*=}"
+      ;;
+     --slug*|-s*)
+      if [[ "$1" != *=* ]]; then shift; fi # Value is next arg if no `=`
+      SLUG="${1#*=}"
+      ;;
+    --help|-h)
+      printf "Meaningful help message" # Flag argument
+      exit 0
+      ;;
+    *)
+      >&2 printf "Error: Invalid argument\n"
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+if [ -z "${VERSION}" ]
+then
+echo 'Version not specified using develop branch'.
+VERSION='develop'
+pwd
+fi
+
+if [ -z "${SLUG}" ]
+then
+SLUG=${PWD##*/}
+echo "Slug not specified using $SLUG".
+pwd
+fi
+
